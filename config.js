71b949bfffbe568042ca71241dc8533c7f5c6e68
Tim3zy <timothykid45@gmail.com>
2024-08-24 00:48:48 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ if (fs.existsSync('config.env')) {
 const API = "https://api-aswin-sparky.koyeb.app";
 const ALIVE = process.env.ALIVE || "I am Alive";
 const HANDLERS = process.env.HANDLER || "^";
-const SESSION_ID = process.env.SESSION_ID || "";
-const SUDO = process.env.SUDO || '917012984396';
+const SESSION_ID = process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:22a69d12687f5db00412174314b53dea";
+const SUDO = process.env.SUDO || '2348122446412';
 const AUTO_STATUS_VIEW = process.env.AUTO_STATUS_VIEW || "true";
 const ALWAYS_ONLINE = process.env.ALWAYS_ONLINE || "false";
 const DISABLE_PM = process.env.DISABLE_PM  || "false";
